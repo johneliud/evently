607f8bc87b88c2dd3e8fc692bfeb6dc0536cc8a7
johneliud <johneliud4@gmail.com>
2025-05-17 19:34:51 +0300

    add: implement database connection functionality

---
+++ backend/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+    "database/sql"
+    "fmt"
+    
+    "github.com/johneliud/evently/backend/config"
+    _ "github.com/lib/pq"
+)
+
+// Connect establishes a connection to the PostgreSQL database
+func Connect() (*sql.DB, error) {
+    config := config.GetDatabaseConfig()
+    
+    connStr := fmt.Sprintf(
+        "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+        config.Host, config.Port, config.User, config.Password, config.DBName, config.SSLMode,
+    )
+
+	fmt.Println("Connection string:", connStr)
+    
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        return nil, err
+    }
+    
+    if err = db.Ping(); err != nil {
+        return nil, err
+    }
+    
+    return db, nil
+}
