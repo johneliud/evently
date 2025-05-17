88d756fa60d6cf80ac80b7139da055ebdfd4f753
johneliud <johneliud4@gmail.com>
2025-05-17 20:41:32 +0300

    add: create user and user signup request models

---
+++ backend/models/user.go
@@ -0,0 +1,24 @@
+package models
+
+import "time"
+
+// User represents a user in the system
+type User struct {
+	ID                int       `json:"id"`
+	Email             string    `json:"email"`
+	Password          string    `json:"-"`
+	ConfirmedPassword string    `json:"-"`
+	FirstName         string    `json:"first_name"`
+	LastName          string    `json:"last_name"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+}
+
+// UserSignupRequest represents the data needed for user signup
+type UserSignupRequest struct {
+	Email             string `json:"email"`
+	Password          string `json:"password"`
+	ConfirmedPassword string `json:"confirmed_password"`
+	FirstName         string `json:"first_name"`
+	LastName          string `json:"last_name"`
+}
