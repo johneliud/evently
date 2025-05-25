5507c772893621dfaca96d991b3a1970a00aa55c
johneliud <johneliud4@gmail.com>
2025-05-25 12:10:33 +0300

    refactor: enhance event and RSVP models with additional fields for organizer and user information

--- backend/models/event.go
+++ backend/models/event.go
@@ -4,14 +4,17 @@ import "time"
 
 // Event represents an event in the system
 type Event struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Date        time.Time `json:"date"`
-	Location    string    `json:"location"`
-	UserID      int       `json:"user_id"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID                 int       `json:"id"`
+	Title              string    `json:"title"`
+	Description        string    `json:"description"`
+	Date               time.Time `json:"date"`
+	Location           string    `json:"location"`
+	UserID             int       `json:"user_id"`
+	CreatedAt          time.Time `json:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at"`
+	OrganizerEmail     string    `json:"organizer_email,omitempty"`
+	OrganizerFirstName string    `json:"organizer_first_name,omitempty"`
+	OrganizerLastName  string    `json:"organizer_last_name,omitempty"`
 }
 
 // EventWithOrganizer extends Event with organizer information
--- backend/models/user.go
+++ backend/models/user.go
@@ -4,27 +4,45 @@ import "time"
 
 // User represents a user in the system
 type User struct {
-	ID                int       `json:"id"`
-	Email             string    `json:"email"`
-	Password          string    `json:"-"`
-	ConfirmedPassword string    `json:"-"`
-	FirstName         string    `json:"first_name"`
-	LastName          string    `json:"last_name"`
-	CreatedAt         time.Time `json:"created_at"`
-	UpdatedAt         time.Time `json:"updated_at"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// UserRequest represents the data needed to create a user
+type UserRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
 
 // UserSignupRequest represents the data needed for user signup
 type UserSignupRequest struct {
+	FirstName         string `json:"first_name"`
+	LastName          string `json:"last_name"`
 	Email             string `json:"email"`
 	Password          string `json:"password"`
 	ConfirmedPassword string `json:"confirmed_password"`
-	FirstName         string `json:"first_name"`
-	LastName          string `json:"last_name"`
 }
 
-// UserSignInRequest represents the data needed for user signin
+// UserSignInRequest represents the data needed for user sign in
 type UserSignInRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+// UserResponse represents the data returned after user creation or authentication
+type UserResponse struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
